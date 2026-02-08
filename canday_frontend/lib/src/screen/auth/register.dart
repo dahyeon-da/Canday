528a3c7772d7cab80f2b0bc173f539561fe678f1
dahyeon-da <pop8261@naver.com>
2026-02-08 22:03:11 +0900

    ✨ feat: 회원가입 페이지 생성 완료 #3

--- canday_frontend/lib/src/screen/auth/register.dart
+++ canday_frontend/lib/src/screen/auth/register.dart
@@ -8,8 +8,162 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+//   final userController = Get.put(UserController());
+
+//   // '일기 쓰러가기' 즉 회원가입 버튼을 눌렀을 때 실행되는 함수
+// _submitForm() async {
+//   final String userEmailAdress =
+// }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+          automaticallyImplyLeading: false,
+          backgroundColor: Colors.white,
+          toolbarHeight: 80,
+          title: Row(
+            children: [
+              Container(width: 5),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(height: 5),
+                  Text(
+                    "‘오늘의 마음을 기록해보세요",
+                    style: TextStyle(fontSize: 23, fontFamily: "handWritten1"),
+                  ),
+                  Text(
+                    "당신만의 작은 공간 “사탕조각” ’",
+                    style: TextStyle(fontSize: 23, fontFamily: "handWritten1"),
+                  )
+                ],
+              ),
+            ],
+          )),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // 이메일 입력창
+          Container(
+            margin: EdgeInsets.fromLTRB(15, 0, 15, 0),
+            child: TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                hintText: "이메일",
+                hintStyle: TextStyle(color: Color.fromRGBO(184, 194, 176, 100)),
+                enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                        color: Color.fromRGBO(206, 250, 163, 100), width: 2)),
+                focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                        color: Color.fromRGBO(132, 219, 46, 1), width: 2.5)),
+              ),
+            ),
+          ),
+
+          // 비밀번호 입력창
+          Container(
+            margin: EdgeInsets.fromLTRB(15, 0, 15, 0),
+            child: TextFormField(
+              keyboardType: TextInputType.visiblePassword,
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: "비밀번호",
+                hintStyle: TextStyle(color: Color.fromRGBO(184, 194, 176, 100)),
+                enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                        color: Color.fromRGBO(206, 250, 163, 100), width: 2)),
+                focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                        color: Color.fromRGBO(132, 219, 46, 1), width: 2.5)),
+              ),
+            ),
+          ),
+
+          // 비밀번호 확인 입력창
+          Container(
+            margin: EdgeInsets.fromLTRB(15, 0, 15, 0),
+            child: TextFormField(
+              keyboardType: TextInputType.visiblePassword,
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: "비밀번호 확인",
+                hintStyle: TextStyle(color: Color.fromRGBO(184, 194, 176, 100)),
+                enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                        color: Color.fromRGBO(206, 250, 163, 100), width: 2)),
+                focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                        color: Color.fromRGBO(132, 219, 46, 1), width: 2.5)),
+              ),
+            ),
+          ),
+
+          // 이름 입력창
+          Container(
+            margin: EdgeInsets.fromLTRB(15, 0, 15, 0),
+            child: TextFormField(
+              keyboardType: TextInputType.name,
+              decoration: InputDecoration(
+                hintText: "이름",
+                hintStyle: TextStyle(color: Color.fromRGBO(184, 194, 176, 100)),
+                enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                        color: Color.fromRGBO(206, 250, 163, 100), width: 2)),
+                focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                        color: Color.fromRGBO(132, 219, 46, 1), width: 2.5)),
+              ),
+            ),
+          ),
+
+          // 생일 입력창
+          Container(
+            margin: EdgeInsets.fromLTRB(15, 0, 15, 0),
+            child: TextFormField(
+              keyboardType: TextInputType.datetime,
+              decoration: InputDecoration(
+                hintText: "생일",
+                hintStyle: TextStyle(color: Color.fromRGBO(184, 194, 176, 100)),
+                enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                        color: Color.fromRGBO(206, 250, 163, 100), width: 2)),
+                focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                        color: Color.fromRGBO(132, 219, 46, 1), width: 2.5)),
+              ),
+            ),
+          ),
+          Container(height: 25),
+
+          // 회원가입 완료 버튼 (일기 쓰러가기)
+          Container(
+            width: double.infinity,
+            margin: EdgeInsets.fromLTRB(15, 0, 15, 0),
+            child: ElevatedButton(
+              onPressed: () {},
+              child: Container(
+                child: Text(
+                  "일기 쓰러가기",
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white),
+                ),
+                margin: EdgeInsets.fromLTRB(0, 13, 0, 13),
+              ),
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Color.fromRGBO(193, 240, 146, 1),
+                  shadowColor: Colors.transparent,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  )),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
