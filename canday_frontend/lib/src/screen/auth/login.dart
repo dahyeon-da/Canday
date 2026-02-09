98f3e7a6083ecca1c52f7ab7d32966d217f82a3e
dahyeon-da <pop8261@naver.com>
2026-02-09 21:47:16 +0900

    ✨ feat: 로그인 페이지 생성 #3

--- canday_frontend/lib/src/screen/auth/login.dart
+++ canday_frontend/lib/src/screen/auth/login.dart
@@ -10,6 +10,77 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          backgroundColor: Colors.white,
+          toolbarHeight: 80,
+          title: Row(
+            children: [
+              Container(width: 5),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(height: 5),
+                  Text(
+                    "‘오늘의 마음을 기록해보세요",
+                    style: TextStyle(fontSize: 23, fontFamily: "handWritten1"),
+                  ),
+                  Text(
+                    "당신만의 작은 공간 “사탕조각” ’",
+                    style: TextStyle(fontSize: 23, fontFamily: "handWritten1"),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // 이메일 입력창
+            Container(
+              margin: EdgeInsets.fromLTRB(15, 0, 15, 0),
+              child: TextFormField(
+                // controller: _userEmailAdressController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  hintText: "이메일",
+                  hintStyle:
+                      TextStyle(color: Color.fromRGBO(184, 194, 176, 100)),
+                  enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                          color: Color.fromRGBO(206, 250, 163, 1), width: 1)),
+                  focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                          color: Color.fromRGBO(132, 219, 46, 1), width: 2)),
+                ),
+              ),
+            ),
+
+            // 비밀번호 입력창
+            Container(
+              margin: EdgeInsets.fromLTRB(15, 0, 15, 0),
+              child: TextFormField(
+                // controller: _userPasswordController,
+                keyboardType: TextInputType.visiblePassword,
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: "비밀번호",
+                  hintStyle:
+                      TextStyle(color: Color.fromRGBO(184, 194, 176, 100)),
+                  enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                          color: Color.fromRGBO(206, 250, 163, 1), width: 1)),
+                  focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                          color: Color.fromRGBO(132, 219, 46, 1), width: 2)),
+                ),
+              ),
+            ),
+            Container(height: 20)
+          ],
+        ));
   }
 }
